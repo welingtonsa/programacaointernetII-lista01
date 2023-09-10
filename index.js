ec1d3523d907bc0ecbb90fa0167112c80c9a48b6
Welington Silva Araujo <welington.sa@gmail.com>
2023-09-10 18:12:56 -0300

    Configuracao das rotas e layout das paginas.

--- index.js
+++ index.js
@@ -6,26 +6,46 @@ const app = express();
 /**Port  */
 const port = 3000;
 
+/** Bootstrap files */
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+/* Import todos os arquivos statics*/
+app.use(express.static('./public'))
+
+
 /** Rota para pagina inicial. */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/home.html'));
+    res.sendFile(path.join(__dirname, '/pages/index.html'));
+    
+})
+/** Rota para pagina index. */
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/index.html'));    
 })
 
 /** Rota para about */
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/about.html'))
+    res.sendFile(path.join(__dirname, '/pages/about.html'));
 })
 
 /**Rota para login */
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/login.html'))
+    res.sendFile(path.join(__dirname, '/pages/login.html'));
 })
 
 /** Rota para user */
 app.get('/user', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/user.html'))
+    res.sendFile(path.join(__dirname, '/pages/user.html'));
 })
 
+/** Erro 404 */
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/pages/404.html'));
+})
+
+
 app.listen(port, ()=>{
-    console.log(`Running at port: ${port}`)
+    console.log(`Runnning at port: ${port}`);
 })
